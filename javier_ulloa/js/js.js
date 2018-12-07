366f9100e198e9a6c46e4a17101f46674d65ad4a
Javier Ulloa <javier.ulloa@paris.cl>
2018-12-07 09:39:30 -0300

    subir prueba

---
+++ javier_ulloa/js/js.js
@@ -0,0 +1,57 @@
+  /*$.ajax({
+    url: "https://js-instagram-proxy.herokuapp.com/botavian/media",
+    dataType: "jsonp",
+    data: {
+      count: 12
+  },
+      success: function(json) {
+      for (var i in json.posts) {
+      if(i >= 12){continue}
+      url = json.posts[i].display_url;
+      shortcode = json.posts[i].shortcode;
+
+      newElement = document.createElement('div');
+      newElement.className = 'insta_img';
+      newElement.style = 'background-image: url(' + url + ')';
+
+      newElementLink = document.createElement('a');
+      newElementLink.href = "https://www.instagram.com/p/" + shortcode;
+      newElementLink.appendChild(newElement);
+
+      var clientHeight = document.getElementById('instagram').clientHeight;
+      document.getElementById("instagram").appendChild(newElementLink);
+    }
+  }
+  });
+
+
+  function parse_str(str) {
+    return str.split('&').reduce(function(params, param) {
+      var paramSplit = param.split('=').map(function(value) {
+        return decodeURIComponent(value.replace('+', ' '));
+      });
+      params[paramSplit[0]] = paramSplit[1];
+      return params;
+    }, {});
+  }*/
+
+$.ajax({
+url: 'https://randomuser.me/api/?results=12',
+type: 'GET',
+dataType: 'json',
+})
+.done(function(data) {
+console.log(data.results);
+
+$.each( data.results, function( key, value ) {
+$('.user').append(`<li class="col-md-3 col-sm-3 col-xs-6">
+<img src="${value.picture.large}" class="foto_amigos" alt="">
+<h4 class="titulo_inicio">${value.name.first} ${value.name.last}</h4>
+<a class="user_email">${value.email}</a>
+</li>`)
+});
+})
+.fail(function() {
+console.log("error");
+})
+
